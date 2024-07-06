ab961ceda671a34c38422bba706241f3eb0c8b93
Samyuktha_K <140744495+KSamyu@users.noreply.github.com>
2024-07-06 21:47:04 +0530

    Update GoogleFormRedirector11.java

--- agile/src/agile/GoogleFormRedirector11.java
+++ agile/src/agile/GoogleFormRedirector11.java
@@ -75,9 +75,8 @@ public class GoogleFormRedirector11 extends JFrame {
         //pst.executeUpdate();
         //stmt.execute("update spareparts set sno="+sno.getText()+",partno="+part.getText()+" where itemname='"+itemname.getText()+"'");
        
-     //      String url = "pst";
-               // String url = "https://docs.google.com/forms/d/e/your-form-id/viewform";
-               String url = "https://docs.google.com/forms/d/e/1FAIpQLSd9ObkhQpHbCD8ZF3xdnxXsThbdjNMEd2l9z6VI3bfKzyTCKw/viewform?usp=sf_link";
+   
+               String url = "https://docs.google.com/forms/d/e/";
                try {
                     Desktop.getDesktop().browse(new java.net.URI(url));
                 }
